8cfc5f529ec7bc7dea18817d319cd6bdb1ed5b17
Mario Reid <mario.reid83@gmail.com>
2020-12-09 20:51:01 -0500

    redoing js

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+
+  var generateBtn = document.querySelector("#generate");
+
+var upperCaseCharacters = "ABCDEFGHIJKLMONPQSTUVWXYZ";
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var numericCharacters = "1234567890";
+var specialCharacters = "!@#$%^&*()";
+
+var requireCharacterString = ""
+
+var passwordLength = prompt ("How many characters would you like in your password? \n(Please enter a number between 8-12");
+
+while(passwordLength < 8 || passwordLength > 128){
+  passwordLength = prompt("The password must be between 8 and 128 characters. \nPlease select a number between 8-12.")
+}
+
+var includeLowerCase = confirm("")
+
+
+
+
+
+
+
+var includeUpperCase = confirm("Do you want to include uppercase characters?");
+console.log("Include uppercase characters: " = includeUpperCase);
+
+if(includeUpperCase){
+  requireCharacterString = requireCharacterString.concat(upperCaseCharacters);
+}
+
+var includeNumeric = confirm("Do you want to include numeric characters?");
+console.log("Include numeric characters: " + includeNumeric);
+
+if(inclue)
+
+
+
+
+
+
+
+
+
+
+var passwordArray[]
 
 // Write password to the #password input
 function writePassword() {
@@ -9,9 +55,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-function generatePassword(){
-  return "temporary password"
+generatedPassword = generatedPassword.concat(require)
 }
+  return generatedPassword;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
