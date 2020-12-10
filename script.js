a0c704137b3ef04c39e43dcad8818e7e9d675add
Mario Reid <mario.reid83@gmail.com>
2020-12-10 00:11:44 -0500

    edited left spacing

--- script.js
+++ script.js
@@ -15,86 +15,86 @@ function writePassword() {
 function generatePassword(){
 
 
-  var generatedPassword = "";
+    var generatedPassword = "";
 
 
 
-  var upperCaseCharacters = "ABCDEFGHIJKLMONPQSTUVWXYZ";
-  var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-  var numericCharacters = "1234567890";
-  var specialCharacters = "!@#$%^&*()";
+    var upperCaseCharacters = "ABCDEFGHIJKLMONPQSTUVWXYZ";
+    var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    var numericCharacters = "1234567890";
+    var specialCharacters = "!@#$%^&*()";
 
 
 
-  var requireCharacterString = "";
+    var requireCharacterString = "";
 
 
   var passwordLength = prompt ("How many characters would you like in your password? \n(Please enter a number between 8-12");
 
 
-while(passwordLength < 8 || passwordLength > 128){
-  passwordLength = prompt("The password must be between 8 and 128 characters. \nPlease select a number between 8-12.")
-}
+  while(passwordLength < 8 || passwordLength > 128){
+    passwordLength = prompt("The password must be between 8 and 128 characters. \nPlease select a number between 8-12.")
+  }
 
-//console.log("Password Length: " +passwordLength);
-
-
-var includeLowerCase = confirm("Do you want to include lowercase characters?");
-
-if(includeLowerCase){
-  requireCharacterString = requireCharacterString.concat(lowerCaseCharacters);
-
-  generatedPassword = generatedPassword.concat(Math.floor(Math.random()*lowerCaseCharacters.length));
-}
+  //console.log("Password Length: " +passwordLength);
 
 
-var includeUpperCase = confirm("Do you want to include uppercase characters?");
-console.log("Include uppercase characters: " = includeUpperCase);
+  var includeLowerCase = confirm("Do you want to include lowercase characters?");
 
-if(includeUpperCase){
-requireCharacterString = requireCharacterString.concat(upperCaseCharacters);
+  if(includeLowerCase){
+    requireCharacterString = requireCharacterString.concat(lowerCaseCharacters);
 
-generatedPassword = generatedPassword.concat(Math.floor(Math.random()*upperCaseCharacters.length));
-}
-var includeNumeric = confirm("Do you want to include numeric characters?");
-console.log("Include numeric characters: " + includeNumeric);
+    generatedPassword = generatedPassword.concat(Math.floor(Math.random()*lowerCaseCharacters.length));
+  }
 
 
-if(includeNumeric){
-requireCharacterString = requireCharacterString.concat(numericCharacters);
+  var includeUpperCase = confirm("Do you want to include uppercase characters?");
+  console.log("Include uppercase characters: " = includeUpperCase);
 
-generatedPassword = generatedPassword.concat(Math.floor(Math.random()*numericCharacters.length));
-}
+  if(includeUpperCase){
+  requireCharacterString = requireCharacterString.concat(upperCaseCharacters);
 
-
-var includeSpecial = confirm("Do you want to include special characters?");
-console.log("Include special characters: " +includeSpecial);
-
-if(includeSpecial){
-requireCharacterString = requireCharacterString.concat(specialCharacters);
-
-generatedPassword = generatedPassword.concat(Math.floor(Math.random()*specialCharacters.length));
-}
-
-console.log("Required characters array string: " + requireCharacterString);
-console.log("Generated Password Length : " +generatedPassword.length);
-console.log("Generated Password so far is : "+generatedPassword);
+  generatedPassword = generatedPassword.concat(Math.floor(Math.random()*upperCaseCharacters.length));
+  }
+  var includeNumeric = confirm("Do you want to include numeric characters?");
+  console.log("Include numeric characters: " + includeNumeric);
 
 
+  if(includeNumeric){
+  requireCharacterString = requireCharacterString.concat(numericCharacters);
+
+  generatedPassword = generatedPassword.concat(Math.floor(Math.random()*numericCharacters.length));
+  }
 
 
-for(var i = 0; < passwordLength; i++) {
+  var includeSpecial = confirm("Do you want to include special characters?");
+  console.log("Include special characters: " +includeSpecial);
+
+  if(includeSpecial){
+  requireCharacterString = requireCharacterString.concat(specialCharacters);
+
+  generatedPassword = generatedPassword.concat(Math.floor(Math.random()*specialCharacters.length));
+  }
+
+  console.log("Required characters array string: " + requireCharacterString);
+  console.log("Generated Password Length : " +generatedPassword.length);
+  console.log("Generated Password so far is : "+generatedPassword);
 
 
-  var random = Math.floor(Math.random() * requireCharacterString.length);
 
 
-  generatedPassword = generatedPassword.concat(requiredCharactersString.charAt(random));
-  console.log("Generated password is : "+generatedPassword);
-}
+  for(var i = 0; < passwordLength; i++) {
 
 
-return generatedPassword;
+    var random = Math.floor(Math.random() * requireCharacterString.length);
+
+
+    generatedPassword = generatedPassword.concat(requiredCharactersString.charAt(random));
+    console.log("Generated password is : "+generatedPassword);
+  }
+
+
+  return generatedPassword;
 }
 
 // Add event listener to generate button
